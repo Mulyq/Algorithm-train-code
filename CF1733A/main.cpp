80a7560f38ccf35428f75b852255dd5c5244673e
Muly <466010106@qq.com>
2022-09-21 15:22:28 +0800

    hello

--- CF1733A/main.cpp
+++ CF1733A/main.cpp
@@ -5,12 +5,27 @@ typedef pair<int, int> PII;
 const int INF = 1e9 + 7, MAXN = 2e5 + 10, mod = 998244353;
 
 void solve() {
-    cout << 1 << '\n';
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i ++) {
+        cin >> a[i];
+    }
+    ll res = 0;
+    for(int i = 0; i < k && i < n; i ++) {
+        int t = a[i];
+        for(int j = i; j < n; j += k) {
+            t = max(t, a[j]);
+        }
+        res += t;
+    }
+    cout << res << '\n';
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int _ = 1;
+    cin >> _;
     while(_ --) {
         solve();
     }
