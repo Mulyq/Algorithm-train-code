83988549665329badd08f910fac5a5dbc79a5e49
Muly <466010106@qq.com>
2022-10-08 11:17:31 +0800

    hello

--- CF1733D/main.cpp
+++ CF1733D/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> PII;
 const int INF = 1e9 + 7, MAXN = 2e5 + 10, mod = 998244353;
-
+ll dp[1010][1010];
 void solve() {
     int n, x, y;
     cin >> n >> x >> y;
@@ -30,7 +30,37 @@ void solve() {
                 if(!ok) cout << y << '\n';
                 else cout << min(2ll * y, 1ll * x) << '\n';
             }
+        } else {
+            vector<int> pos(1, 0);
+            for(int i = 1; i <= n; i ++) {
+                if(c[i - 1]) pos.push_back(i);
+            }
+            n = pos.size() - 1;
+            for(int i = 0; i <= n; i ++) {
+                for(int j = 0; j <= n; j ++) {
+                    dp[i][j] = 1e18;
+                }
+            }
+            for(int i = 0; i < n; i ++) {
+                dp[i + 1][i] = 0;
+            }
+            for(int len = 2; len <= n; len += 2) {
+                for(int l = 1; ; l ++) {
+                    int r = l + len - 1;
+                    if(r > n) break;
+                    ll t1 = (pos[l] != pos[r] ? 1ll * y : 1e18), t2 = 1ll * (pos[r] - pos[l]) * x;
+                    ll x1 = dp[l + 1][r - 1] + min(t1, t2);
+                    t1 = (pos[l] != pos[l + 1] ? 1ll * y : 1e18), t2 = 1ll * (pos[l + 1] - pos[l]) * x;
+                    ll x2 = dp[l + 2][r] + min(t1, t2);
+                    t1 = (pos[r] != pos[r - 1] ? 1ll * y : 1e18), t2 = 1ll * (pos[r] - pos[r - 1]) * x;
+                    ll x3 = dp[l][r - 2] + min(t1, t2);
+                    dp[l][r] = min({x1, x2, x3});
+                    // cout << l << ' ' << r << ' ' << dp[l][r] << '\n';
+                }
+            }
+            cout << dp[1][n] << '\n';
         }
+
     }
 }
 int main() {
--- abc271E/main.cpp
+++ abc271E/main.cpp
@@ -6,74 +6,29 @@ const int INF = 1e9 + 7, MAXN = 2e5 + 10, mod = 998244353;
 
 int n, m, k;
 struct Edge {
-    int u, v, val, xl;
+    int a, b, c;
 };
-
-vector<Edge> G[MAXN];
-vector<int> rG[MAXN];
-vector<int> E;
-ll res = 1e18;
-vector<bool> vis;
-vector<int> path;
-bool posib[MAXN];
-void init(int n) {
-    queue<int> q;
-    q.push(n);
-    posib[n] = 1;
-    while(q.size()) {
-        int x = q.front();
-        q.pop();
-        for(int y : rG[x]) if(!posib[y]) {
-            posib[y] = 1;
-            q.push(y);
-        }
-    }
-}
-bool check() {
-    for(int i = 0, j = 0; i < E.size(); i ++) {
-        if(path[j] == E[i]) j ++;
-        if(j >= path.size()) return 1;
-    }
-    return 0;
-}
-void dfs(int x, ll sum) {
-    if(sum >= res || !posib[x]) return;
-    if(x == n) {
-        if(check()) {
-            res = min(res, sum);
-        }
-        return;
-    }
-    vis[x] = 1;
-    for(auto &[u, v, val, xl] : G[x]) if(!vis[v]) {
-        path.push_back(xl);
-        dfs(v, sum + val);
-        path.pop_back();
-    }
-    vis[x] = 0;
-}
+ll dis[MAXN];
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cin >> n >> m >> k;
-    E.resize(k);
-    vis.resize(n + 1, 0);
-    for(int i = 0; i < m; i ++) {
-        int a, b, c;
+    for(int i = 0; i < n; i ++) {
+        dis[i] = 1e18;
+    }
+    vector<Edge> G(m);
+    for(auto &[a, b, c] : G) {
         cin >> a >> b >> c;
-        G[a].push_back({a, b, c, i});
-        rG[b].push_back(a);
+        a --, b --;
     }
-    init(n);
+    dis[0] = 0;
     for(int i = 0; i < k; i ++) {
-        int x;
-        cin >> x;
-        E[i] = x - 1;
+        int e;
+        cin >> e;
+        e --;
+        auto [a, b, c] = G[e];
+        dis[b] = min(dis[b], dis[a] + c);
     }
-    dfs(1, 0);
-    if(res == 1e18) {
-        cout << "-1\n";
-    } else {
-        cout << res << '\n';
-    }
+    if(dis[n - 1] == 1e18) cout << -1 << '\n';
+    else cout << dis[n - 1] << '\n';
 }
